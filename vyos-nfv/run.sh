4ec573b7f8a27dda6795cd6a5d76edf6e22f79cb
Yan Grunenberger <ravens@users.noreply.github.com>
2019-03-22 16:48:13 +0100

    Update run.sh

--- vyos-nfv/run.sh
+++ vyos-nfv/run.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 ip tuntap add mode tap tap0
-
 ip link set tap0 master br0
-
 ip link set dev br0 up
-
 ip link set dev tap0 up
-
 /usr/bin/qemu-system-x86_64 -nographic -serial mon:stdio  -device virtio-net,netdev=network0,mac=52:55:00:d1:55:01 -netdev tap,id=network0,ifname=tap0,script=no,downscript=no -m 512 -smp 1 -machine type=pc,accel=kvm -drive file=/root/vyos.img,if=virtio,cache=writeback,discard=ignore,format=qcow2 -boot once=d
-
-# text console using 

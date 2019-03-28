e105cb23dd73bd7fac0a62a837631a460b0a9f2d
Yan Grunenberger <yan.grunenberger@telefonica.com>
2019-03-28 17:11:29 +0100

    Adding auth API from nodejs

--- cloudserver/index.js
+++ cloudserver/index.js
@@ -6,3 +6,27 @@ http.createServer(function (request, response) {
 }).listen(3000);
 
 console.log('Server started');
+
+const Salt = require("salt-api");
+
+const salt = new Salt({
+	url: "http://192.168.25.7:8080/hook",
+	username: "labuser",
+	password: "labpassword"
+});
+
+
+salt.ready.then(() => {
+
+	console.log("Salt ready...")
+
+	/*// Same as running `salt "*" test.ping` in the command line
+	salt.fun("*", "test.ping").then(data => {
+
+		// Do something with the data
+		console.log(data);
+		// { return: [ { b827eb3aaaf7: true, b827ebcc82fe: true } ] }
+
+	}).catch(e => console.error(e));*/
+
+});

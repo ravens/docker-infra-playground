507c7e07499eaa0802add5251b0be7795405e9bc
Yan Grunenberger <yan.grunenberger@telefonica.com>
2019-03-28 16:23:43 +0100

    Adding a homemade web controller for Salt

---
+++ cloudserver/index.js
@@ -0,0 +1,8 @@
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server started');

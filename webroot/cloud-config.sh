3a34af31ccaf4d6d6a1faf0a9924cda5574c8112
Yan Grunenberger <yan@grunenberger.net>
2019-09-20 17:29:50 +0200

    Script that autoinstall ubuntu

--- webroot/cloud-config.sh
+++ webroot/cloud-config.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 
-docker run --rm mwendler/wget http://192.168.25.3/404
+wget -O /tmp/ubuntu.qcow2 https://cloud-images.ubuntu.com/bionic/current/bionic-server-cloudimg-amd64.img
+docker run -v /tmp:/tmp --device /dev/sda anishs/qemu-img convert /tmp/ubuntu.qcow2 -O raw /dev/sda
+reboot
